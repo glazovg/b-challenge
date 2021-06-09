8fc6724b4da65d95c9f7eb4e3c4f9ad3d3055b16
Guillermo Lazo <jlazodg@jci.com>
2021-06-09 18:05:45 -0500

    Adding validations for all cryptos

--- test/bitso.test.js
+++ test/bitso.test.js
@@ -22,6 +22,7 @@ dataSet.forEach(data => {
         it(`Adding a beneficiary - ${data.scenario}`, async () => {
             LoginPage.open()
             LoginPage.login(data.email, data.password)
+            
             if (await LoginPage.isAuthNewDeviceMessageDisplayed()) {
                 const email = await mailslurp.waitForLatestEmail(data.inboxId);
                 const regex = RegExp('https:\/\/devmalta\.bitso\.com\/auth_device\/[a-zA-Z0-9]*')
@@ -29,12 +30,15 @@ dataSet.forEach(data => {
                 console.log('\x1b[32m%s\x1b[0m', `AUTH URL: ${authPath}`)
                 page.open(authPath)
             }
-            WalletPage.open()
-            await browser.pause(3000)
-            await WalletPage.cryptoDeposit(1)
-            expect((await WalletPage.webElements()).alertDepositMessage).toBeDisplayed()
-            await browser.pause(5000)
-            await browser.keys('Escape')
+            
+            await browser.pause(7000)
+
+            for (let i = 1; i < 9; i++) {
+                await WalletPage.cryptoDeposit(i)
+                expect((await WalletPage.webElements()).alertDepositMessage).toBeDisplayed()
+                await browser.pause(4000)
+                await browser.keys('Escape')
+            }
         })
     })
 });
