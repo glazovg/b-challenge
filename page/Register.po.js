1e4b3e9be4f08601e3916454d6d4896bddbbd39a
Guillermo Lazo <jlazodg@jci.com>
2021-06-09 01:08:24 -0500

    Adding data provider for Create account test

--- page/Register.po.js
+++ page/Register.po.js
@@ -2,23 +2,28 @@ import Page from './Page.po'
 
 class Register extends Page {
     async webElements() {
+        const countryResidence = await $('#country')
         const emailInput = await $('#email')
         const passwordInput = await $('#password')
         const passwordConfirmation = await $('#password_confirmation')
         const registerCheckBox = await $$('.moon-val_check')
         const startButton = await $('button[type="submit"]=Start')
         const codeInput = await $('[inputMode="numeric"]')
-        return { emailInput, passwordInput, passwordConfirmation, registerCheckBox, startButton, codeInput }
+        return { countryResidence, emailInput, passwordInput, passwordConfirmation, registerCheckBox, startButton, codeInput }
     }
 
-    async createAccount(email, password) {
+    async createAccount(country, email, password) {
+        if (country !== 'Mexico') {
+            await (await this.webElements()).countryResidence.setValue(country)
+            await browser.keys('Enter')
+        }
         await (await this.webElements()).emailInput.setValue(email)
         await (await this.webElements()).passwordInput.setValue(password)
         await (await this.webElements()).passwordConfirmation.setValue(password)
         await (await this.webElements()).registerCheckBox[0].scrollIntoView()
         await browser.execute("arguments[0].click()", (await this.webElements()).registerCheckBox[0])
         await browser.execute("arguments[0].click()", (await this.webElements()).registerCheckBox[2])
-        await browser.execute("arguments[0].click()", (await this.webElements()).registerCheckBox[3])
+        if (country === 'Mexico') await browser.execute("arguments[0].click()", (await this.webElements()).registerCheckBox[3])
         await (await this.webElements()).startButton.waitForEnabled()
         await browser.execute("arguments[0].click()", (await this.webElements()).startButton)
     }
--- test/register.test.js
+++ test/register.test.js
@@ -1,19 +1,25 @@
 import MailSlurp from 'mailslurp-client'
 import RegisterPage from '../page/Register.po'
 import apiKey from '../mailslurp.conf'
+
+const dataSet = require('../dataProvider/register-data.json')
 const mailslurp = new MailSlurp({ apiKey });
 
 let inbox;
-let password = "Test-password1";
 
-describe('Register page', () => {
-    it('Creating an Individual account', async () => {
-        inbox = await mailslurp.createInbox()
-        RegisterPage.open()
-        RegisterPage.createAccount(inbox.emailAddress,password)
-        const email = await mailslurp.waitForLatestEmail(inbox.id);
-        const code = /[0-9]{6}/.exec(email.body);
-        RegisterPage.verifyCode(code)
-        await browser.pause(2000)
+dataSet.forEach(data => {
+    describe('Register page', () => {
+        after(async () =>{
+            await browser.reloadSession()
+        })
+        it(`Creating an Individual account - ${data.scenario}`, async () => {
+            inbox = await mailslurp.createInbox()
+            RegisterPage.open()
+            RegisterPage.createAccount(data.country, inbox.emailAddress, data.password)
+            const email = await mailslurp.waitForLatestEmail(inbox.id);
+            const code = /[0-9]{6}/.exec(email.body);
+            RegisterPage.verifyCode(code)
+            await browser.pause(2000)
+        })
     })
 })
